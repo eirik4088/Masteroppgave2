d4ed105053de4135519f50c4fb891c906cc7f036
eirik4088 <eirik.s.gulbrandsen@gmail.com>
2024-04-03 12:31:22 +0200

    yep

--- scripts/mpi_lemon_eval.py
+++ scripts/mpi_lemon_eval.py
@@ -97,7 +97,7 @@ def evaluate(processor, to_fill: np.ndarray, baseline=None):
         to_fill[1:] = float("nan")
 
 
-def evaluate_autorej(epochs, bad_channels, to_fill: np.ndarray, baseline=None):
+def evaluate_other(epochs, bad_channels, to_fill: np.ndarray, baseline=None):
 
     if len(bad_channels) > 0:
         to_fill[0] = len(bad_channels) / 128
@@ -196,9 +196,9 @@ def process(my_index):
             bads_name = [epochs_copy.ch_names[idx] for idx in bads_index]
             epochs_copy.drop_channels(bads_name)
         else:
-            bads_name = []
+            bads_name_autorej = []
 
-        evaluate_autorej(epochs_copy, bads_name, results[1, :], base_line)
+        evaluate_other(epochs_copy, bads_name_autorej, results[1, :], base_line)
 
         # Then implement pyprep
         montage_kind = "biosemi64"
@@ -215,69 +215,72 @@ def process(my_index):
 
         prep = PrepPipeline(raw_copy, prep_params, montage)
         prep.fit()
+        bads_name_prep = prep.interpolated_channels + prep.still_noisy_channels
+        evaluate_other(epochs.copy(), bads_name_prep, results[2, :], base_line)
 
         prep = PrepPipeline(raw_copy, prep_params, montage, matlab_strict=True, random_state=435656)
         prep.fit()
+        bads_name_prep_mat = prep.interpolated_channels + prep.still_noisy_channels
+        evaluate_other(epochs.copy(), bads_name_prep_mat, results[3, :], base_line)
 
 
         save_folder = pathlib.Path(
             r"C:\Users\workbench\eirik_master\Results\epi_data\results_run_2"
         )
         np.save(save_folder / str(eye) / "base_line" / f"{my_index}", base_line)
-        np.save(save_folder / str(eye) / "quasi" / f"{my_index}", quasi_results)
-        np.save(save_folder / str(eye) / "peak" / f"{my_index}", peak_results)
-        np.save(save_folder / str(eye) / "combined" / f"{my_index}", combined_results)
+        np.save(save_folder / str(eye) / "results" / f"{my_index}", results)
 
 
 # Run experiments
 if __name__ == "__main__":
-    p0 = Process(target=process, args=(0,))
-    p1 = Process(target=process, args=(1,))
-    p2 = Process(target=process, args=(2,))
-    p3 = Process(target=process, args=(3,))
-    p4 = Process(target=process, args=(4,))
-    p5 = Process(target=process, args=(5,))
-    p6 = Process(target=process, args=(6,))
-    p7 = Process(target=process, args=(7,))
-    p8 = Process(target=process, args=(8,))
-    p9 = Process(target=process, args=(9,))
-    p10 = Process(target=process, args=(10,))
-    p11 = Process(target=process, args=(11,))
-    p12 = Process(target=process, args=(12,))
-    p13 = Process(target=process, args=(13,))
-    p14 = Process(target=process, args=(14,))
-    p15 = Process(target=process, args=(15,))
+    for i in range(0, 211, 16):
+        p0 = Process(target=process, args=(i,))
+        p1 = Process(target=process, args=(i+1,))
+        p2 = Process(target=process, args=(i+2,))
+        p3 = Process(target=process, args=(i+3,))
+        p4 = Process(target=process, args=(i+4,))
+        p5 = Process(target=process, args=(i+5,))
+        p6 = Process(target=process, args=(i+6,))
+        p7 = Process(target=process, args=(i+7,))
+        p8 = Process(target=process, args=(i+8,))
+        p9 = Process(target=process, args=(i+9,))
+        p10 = Process(target=process, args=(i+10,))
+        p11 = Process(target=process, args=(i+11,))
+        p12 = Process(target=process, args=(i+12,))
+        p13 = Process(target=process, args=(i+13,))
+        p14 = Process(target=process, args=(i+14,))
+        p15 = Process(target=process, args=(i+15,))
 
-    p0.start()
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    p5.start()
-    p6.start()
-    p7.start()
-    p8.start()
-    p9.start()
-    p10.start()
-    p11.start()
-    p12.start()
-    p13.start()
-    p14.start()
-    p15.start()
+        p0.start()
+        p1.start()
+        p2.start()
+        p3.start()
+        p4.start()
+        p5.start()
+        p6.start()
+        p7.start()
+        p8.start()
+        p9.start()
+        p10.start()
+        p11.start()
+        p12.start()
+        p13.start()
+        p14.start()
+        p15.start()
 
-    p0.join()
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    p7.join()
-    p8.join()
-    p9.join()
-    p10.join()
-    p11.join()
-    p12.join()
-    p13.join()
-    p14.join()
-    p15.join()
+        p0.join()
+        p1.join()
+        p2.join()
+        p3.join()
+        p4.join()
+        p5.join()
+        p6.join()
+        p7.join()
+        p8.join()
+        p9.join()
+        p10.join()
+        p11.join()
+        p12.join()
+        p13.join()
+        p14.join()
+        p15.join()
